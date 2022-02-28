2102777a27e22189baff00bf7d92a97d67f97423
KarlitosTaquitos <chern4261@gmail.com>
2022-02-28 14:20:40 -0600

    Updated visuals

--- src/main/java/com/revature/songdex/service/SWService.java
+++ src/main/java/com/revature/songdex/service/SWService.java
@@ -11,17 +11,26 @@ import java.util.Locale;
  */
 public class SWService {
     SWRepo repo;
+    String inlineStyle;
 
     /**
      * SWService constructor takes a SWRepo that it will use as its repository
+     *
      * @param repo Repository of charcters to use
      */
     public SWService(SWRepo repo) {
+        this.inlineStyle = "<style>\n" +
+                "body {background-color: #E0FFFF;}\n" +
+                "h1 {font-weight: bold; font-style: italic;}\n" +
+                "form {font-weight: bold; text-decoration: underline; text-decoration-color: black;}\n" +
+                "a {color: black; text-decoration: none;}" +
+                "</style>\n";
         this.repo = repo;
     }
 
     /**
      * Gets all characters
+     *
      * @return List of Persons objects holding all the characters in repo
      */
     public List<Person> getPerson() {
@@ -30,6 +39,7 @@ public class SWService {
 
     /**
      * Searches for a specific character by name
+     *
      * @param input Name of character being searched for
      * @return Person object with matching name
      */
@@ -39,12 +49,14 @@ public class SWService {
 
     /**
      * Creates the HTML Header for the Search Page
+     *
      * @return String of html header
      */
     public String searchHeader() {
         String HTMLHeader = "<html>\n" +
                 "<meta charset='UTF-8'/>\n" +
-                "<head><title>Search Star Wars Characters</title></head>\n" +
+                "<head><title>Search Star Wars Characters</title>" +
+                inlineStyle +
                 "<body>\n" +
                 "    <form action='list' method='get'>\n" +
                 "        <input type='submit' value='Full Character List'/>\n" +
@@ -57,15 +69,16 @@ public class SWService {
 
     /**
      * Creates the HTML statistics for specific character
+     *
      * @param p Person object to create statistics from
      * @return String of HTML statistics
      */
     public String infoPage(Person p) {
         String HTMLStats = "<html>\n" +
                 "<body>\n" +
-                "<h1>" + p.getName() +  "</h1>\n" +
+                "<h1>" + p.getName() + "</h1>\n" +
                 "<p>Height: " + p.getHeight() + "</p>\n" +
-                "<p>Mass: " +  p.getMass() + "</p>\n" +
+                "<p>Mass: " + p.getMass() + "</p>\n" +
                 "<p>Hair Color: " + p.getHairColor() + "</p>\n" +
                 "<p>Skin Color: " + p.getSkinColor() + "</p>\n" +
                 "<p>Eye Color: " + p.getEyeColor() + "</p>\n" +
@@ -81,20 +94,23 @@ public class SWService {
 
     /**
      * Creates header for list page
+     *
      * @return String of HTML header
      */
     public String listHeader() {
         String HTMLListHead = "<html>\n" +
                 "<meta charset='UTF-8'/>\n" +
-                "<head><title>Star Wars Character List</title></head>\n" +
+                "<head><title>Star Wars Character List</title>" +
+                inlineStyle +
+                "</head>\n" +
                 "<body>\n" +
                 "<h1>Character List</h1>\n" +
                 "<form action='sort' method='get'>\n" +
                 "    Sort by Field:<br/>\n" +
-                "    <input type='radio' name='option' value='name'/>Name\n" +
-                "    <input type='radio' name='option' value='height'/>Height\n" +
-                "    <input type='radio' name='option' value='mass'/>Mass\n" +
-                "    <input type='radio' name='option' value='birthYear'/>Birth Year\n" +
+                "    <input type='radio' name='option' value='name' class='opt'/>Name\n" +
+                "    <input type='radio' name='option' value='height' class='opt'/>Height\n" +
+                "    <input type='radio' name='option' value='mass' class='opt'/>Mass\n" +
+                "    <input type='radio' name='option' value='birthYear' class='opt'/>Birth Year</br>\n" +
                 "    <input type='submit' value='Sort'/>\n" +
                 "</form>\n" +
                 "<form action='search' method='get'>\n" +
@@ -108,6 +124,7 @@ public class SWService {
 
     /**
      * Generates an HTML list of all characters as links to their stats pages
+     *
      * @return String of HTML List
      */
     public String listContents() {
@@ -123,17 +140,20 @@ public class SWService {
 
     /**
      * Generates a header for the Sorting pages
+     *
      * @return String of HTML Header
      */
     public String sortHeader(String opt) {
         String HTMLHeader = "<html>\n" +
                 "<meta charset='UTF-8'/>\n" +
-                "<head><title>Sorted Star Wars Characters</title></head>\n" +
+                "<head><title>Sorted Star Wars Characters</title>" +
+                inlineStyle +
+                "</head>\n" +
                 "<body>\n" +
+                "    <h1>Star Wars Characters Sorted By Field: " + opt + "</h1>" +
                 "    <form action='list' method='get'>\n" +
                 "        <input type='submit' value='Full Character List'/>\n" +
                 "    </form>\n" +
-                "    <h1>Star Wars Characters Sorted By Field: " + opt + "</h1>" +
                 "</body>\n" +
                 "</html>";
 
@@ -142,12 +162,13 @@ public class SWService {
 
     /**
      * Generates HTML for sorted list
+     *
      * @param option what kind of sorting is being done
      * @return String of HTML list
      */
     public String sortList(String option) {
         List<Person> peeps;
-        switch(option) {
+        switch (option) {
             case "height":
                 peeps = repo.getHeightSort();
                 break;
